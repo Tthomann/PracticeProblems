390b10aa65df489d5fb2f232085665934e15acb7
Tyler Thomann <Tyler Thomann>
2016-03-21 14:26:33 -0500

    fourth problem complete

---
+++ Problem4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Problem4
+    {
+        List<int> BeginNumbers = new List<int> { 1, 2, 3 };
+
+        List<int> EndNumbers = new List<int>();
+        public Problem4()
+        {
+
+        }
+        public void CycleNumbers()
+        {
+            EndNumbers.Add(BeginNumbers[0]);
+            EndNumbers.Add(BeginNumbers[2]);
+            EndNumbers.Add(BeginNumbers[1]);
+   
+
+            for(int i = 0; i < EndNumbers.Count; i ++)
+            {
+                Console.WriteLine(EndNumbers[i] + 5);
+                Console.ReadLine();
+            }
+        }
+    }
+}
