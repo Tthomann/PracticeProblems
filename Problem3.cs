d96fe81c67a5169968f5a3b587c4336ebcf1588f
Tyler Thomann <Tyler Thomann>
2016-03-21 12:22:49 -0500

    thrid problem complete

---
+++ Problem3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Problem3
+    {
+        List<string> ComboPattern = new List<string>();
+        public Problem3()
+        {
+
+        }
+        public void GoCombo()
+        {
+            ComboPattern.Add("UP");
+            ComboPattern.Add("DOWN");
+            ComboPattern.Add("LEFT");
+            ComboPattern.Add("LEFT");
+            ComboPattern.Add("RIGHT");
+            ComboPattern.Add("RIGHT");
+            ComboPattern.Add("DOWN");
+            ComboPattern.Add("SPIN");
+            ComboPattern.Add("DOWN");
+            ComboPattern.Add("SPIN");
+            ComboPattern.Add("DOWN");
+            ComboPattern.Add("UP");
+            ComboPattern.Add("LEFT");
+            ComboPattern.Add("RIGHT");
+            ComboPattern.Add("LEFT");
+            ComboPattern.Add("DOWN");
+            ComboPattern.Add("SPIN");
+            ComboPattern.Add("UP");
+            ComboPattern.Add("DOWN");
+            ComboPattern.Add("JUMP");
+        }
+        public void PrintTheCombo()
+        {
+            for(int i = 0; i < ComboPattern.Count; i++)
+            {
+                Console.WriteLine(ComboPattern[i]);
+             
+              
+            }
+            
+        }
+    }
+}
