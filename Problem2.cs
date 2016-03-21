913d0c35b7150da1c9380f1efde090ad9705c334
Tyler Thomann <Tyler Thomann>
2016-03-21 11:55:57 -0500

    problem 2 complete

---
+++ Problem2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Problem2
+    {
+        public Problem2()
+        {
+           
+        }
+        public double printPi()
+        {
+           double pi = 3.14;
+            Console.WriteLine("THE VALUE OF PI IS " + pi);
+            return pi;
+        }
+
+    }
+}
