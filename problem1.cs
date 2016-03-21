183d30b2103e2c9cdd38b626fc3a802e8f7b09b1
Tyler Thomann <Tyler Thomann>
2016-03-21 11:46:29 -0500

    first problem complete

---
+++ problem1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class problem1
+    {
+        List<string> InputString = new List<string>{"T", "Y", "L", "E", "R"};
+
+        public problem1()
+        {
+        }
+        public void PrintString()
+        {
+            Console.WriteLine(InputString.Count);
+        }
+    }
+}
